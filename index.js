ffba30286484c7e5e601d69880ebeec675ddf695
maks099 <mrusin099@gmail.com>
2022-05-31 19:45:34 +0300

    get students by group (server)

--- index.js
+++ index.js
@@ -54,9 +54,11 @@ app.get('/addGroup', groupController.show)
 app.post('/addGroup', groupController.addGroup)
 app.get('/addStudent', studentController.show)
 app.post('/addStudent', studentController.addStudent)
-/*  new functions   */
 app.get('/getStudentById/:student_id', studentController.getById)
 app.post('/updateStudentForm', studentController.updateForm)
+
+/*  new functions   */
+app.post('/showAllStudentsInGroup', studentController.getByGroupId)
 
 
 // forms work
